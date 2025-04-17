b91578863432d1f906feaeaf1c0196d061d1c7b9
caimath <mathias.carsault@std.heh.be>
2025-04-17 23:40:55 +0200

    refactor: reorganisation des scripts dans un dossier scripts

--- scripts/script.js
+++ scripts/script.js
@@ -114,42 +114,4 @@ function masquerPourImpression() {
 }
 
 // Appel fonction pour masquer impression
-masquerPourImpression();
-
-// PraticlesJS, particule en mouvement dans le header
-particlesJS("particles-js", {
-    "particles": {
-      "number": { "value": 60, "density": { "enable": true, "value_area": 800 }},
-      "color": { "value": "#ffffff" },
-      "shape": { "type": "circle" },
-      "opacity": { "value": 0.5 },
-      "size": { "value": 3 },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "out_mode": "out"
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": { "enable": true, "mode": "grab" },
-        "onclick": { "enable": true, "mode": "push" },
-        "resize": true
-      },
-      "modes": {
-        "grab": { "distance": 140, "line_linked": { "opacity": 0.8 }},
-        "push": { "particles_nb": 4 }
-      }
-    },
-    "retina_detect": true
-  });
-  
+masquerPourImpression(); 
